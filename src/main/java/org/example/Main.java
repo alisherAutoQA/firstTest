75d095036be7136b9fe480dc10b321268d4bb0ca
Alisher02 <alisherabdullaev02@gmail.com>
2024-07-27 23:49:48 +0500

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,18 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://google.com");
-        driver.getTitle();
+        WebElement input = driver.findElement(By.xpath("//*[@id=\"APjFqb\"]"));
+        input.sendKeys("hello");
+        List<WebElement> inputs = driver.findElements(By.tagName("a"));
+        System.out.println(inputs.size());
 
 
     }
