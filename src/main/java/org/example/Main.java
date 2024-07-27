5be9d355b9cd1a4b6631e48bc67d49b77ff33dd1
Alisher02 <alisherabdullaev02@gmail.com>
2024-07-27 23:14:17 +0500

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+
+    }
+}
+
