9d74f2d5cd82f962d66a9ea04ffdc58b4b30c86c
Alishok <alisherabdullaev010203@gmail.com>
2024-07-31 00:45:16 +0500

    commit for new branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,18 +5,38 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://google.com");
+//        WebElement element = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+//        element.sendKeys("hello world");
 
-        WebElement element = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
-        element.sendKeys("hello world", Keys.ENTER);
+        try{
+
+            driver.get("https://crossbrowsertesting.github.io/drag-and-drop.html");
+            Thread.sleep(2000);
+            WebElement element = driver.findElement(By.id("draggable"));
+            WebElement element2 = driver.findElement(By.id("droppable"));
+
+            Actions actions = new Actions(driver);
+            actions.moveToElement(element).clickAndHold().moveToElement(element2).release().build().perform();
+
+
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        finally {
+            Thread.sleep(2000);
+            driver.quit();
+        }
+
+//
 
         driver.manage().window().maximize();
     }
