d96f451f2164c51e98c5fe3acdbcd8810c8734bf
Alisher02 <alisherabdullaev02@gmail.com>
2024-07-30 22:23:03 +0500

    getText commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,10 @@
 package org.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
@@ -14,12 +13,10 @@ public class Main {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.avito.ru/all/transport");
+        driver.get("https://google.com");
 
-        WebElement element = driver.findElement(By.cssSelector(".page-title-count-wQ7pG"));
-
-        String par = element.getText();
-        System.out.println(par);
+        WebElement element = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+        element.sendKeys("hello world", Keys.ENTER);
 
         driver.manage().window().maximize();
     }
