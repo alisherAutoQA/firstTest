c9e8e55e2cfcfd9dd6e5876642dd0a884eae129b
Alisher02 <alisherabdullaev02@gmail.com>
2024-07-31 21:55:14 +0500

    comm

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Set;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.alertIsPresent;
 
@@ -13,38 +14,35 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
         WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
         try {
 
             driver.get("http://127.0.0.1:5500/index.html");
             Thread.sleep(2000);
 
-            WebElement element = driver.findElement(By.id("a"));
-            WebElement element2 = driver.findElement(By.id("b"));
-            WebElement element3 = driver.findElement(By.id("c"));
+            String window1 = driver.getWindowHandle();
 
+            js.executeScript("window.open()");
 
-            element.click();
-            Alert alert = wait.until(alertIsPresent());
-            alert.accept();
+            Set<String> currentWin = driver.getWindowHandles();
+            String window2 = null;
 
+            for (String window :currentWin) {
+                if(!window.equals(window1)){
+                    window2 = window;
+                    break;
+                }
+            };
 
-            element2.click();
-            Alert prompt = wait.until(alertIsPresent());
-            prompt.sendKeys("Super");
-            prompt.accept();
+            driver.switchTo().window(window2);
+            driver.get("http://127.0.0.1:5500/index.html");
+            driver.close();
+            driver.switchTo().window(window1);
 
-
-            element.click();
-            Alert alert2 = wait.until(alertIsPresent());
-            Thread.sleep(4000);
-            alert2.accept();
-
-
-            element3.click();
-            Alert alert3 = wait.until(alertIsPresent());
-            alert3.dismiss();
 
         }catch (InterruptedException e){
             e.printStackTrace();
